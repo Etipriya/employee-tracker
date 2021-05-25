ed68690288fd3386a894e6fb8622a21aba3a9712
Eti Priya <etipriya@gmail.com>
2021-05-26 00:09:18 +0100

    added role.sql

---
+++ seedRoles.sql
@@ -0,0 +1,11 @@
+USE company_db;
+
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Representative', '25000.00', '1');
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Manager', '35000.00', '1');
+
+INSERT INTO role (title, salary, department_id) VALUES ('Payroll Advisor', '30000.00', '2');
+
+INSERT INTO role (title, salary, department_id) VALUES ('Regional Manager', '65000.00', '3');
+INSERT INTO role (title, salary, department_id) VALUES ('Senior HR Advisor', '55000.00', '3');
+INSERT INTO role (title, salary, department_id) VALUES ('HR Advisor', '45000.00', '3');
+INSERT INTO role (title, salary, department_id) VALUES ('Associate HR Advisor', '23000.00', '3');

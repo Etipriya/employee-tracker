b99107e27b5948c52e09530a0df28404cb7b4398
Eti Priya <etipriya@gmail.com>
2021-05-26 00:24:08 +0100

    added employee.sql

---
+++ seedEmployees.sql
@@ -0,0 +1,11 @@
+USE company_db;
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('John', 'Smith', '7');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Alice', 'Walker', '7');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Harry', 'Potter', '6');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Captain', 'America', '5');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Michael', 'Scott', '4');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('David', 'Brent', '2');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Linda', 'Green', '2');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Ryan', 'Gosling', '3');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Tony', 'Stark', '1');

f2c2cb3206e6d7e291c0f181d1c67672eac64f2d
Eti Priya <etipriya@gmail.com>
2021-05-26 18:10:25 +0100

    added initial files and package.json

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Dependencies
+const mysql = require("mysql");
+const express = require("express");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+//Declare the port and establish server
+const connection = mysql.createConnection({
+  host: "localhost",
+  PORT = 3000,
+  user: "root",
+  password: "root",
+  database: "company_db"
+});
+
+//Listener
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Navigate to localhost: ${PORT}`);
+});

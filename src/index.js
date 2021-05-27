2a91e80a620c68972e0de17bfe57143f7d0bd96f
Eti Priya <etipriya@gmail.com>
2021-05-27 22:49:31 +0100

    added async await functions for department, employee and role

--- src/index.js
+++ src/index.js
@@ -22,33 +22,137 @@ async function init() {
     name: "options",
     message: "What would you like to do?",
     choices: [
-      "View All Employees",
-      "View All Departments",
-      "View All Roles",
-      "View All Employees By Department",
-      "View All Employees By Manager",
-      "Add Employee",
-      "Add Department",
-      "Add Role",
-      "Remove Employee",
-      "Remove Department",
-      "Remove Role",
-      "Update Employee",
-      "Update Employee Role",
-      "Update Employee Manager",
-      "View Total Budget By Department",
-      "Exit",
+      // "View All Employees",
+      // "View All Departments",
+      // "View All Roles",
+      // "View All Employees By Department",
+      // "View All Employees By Manager",
+      // "Add Employee",
+      // "Add Department",
+      // "Add Role",
+      // "Remove Employee",
+      // "Remove Department",
+      // "Remove Role",
+      // "Update Employee",
+      // "Update Employee Role",
+      // "Update Employee Manager",
+      // "View Total Budget By Department",
+      // "Exit",
+      {
+        short: "Employees",
+        value: "viewAllEmployees",
+        name: "View All Employees",
+      },
+      {
+        short: "Employees By Department",
+        value: "viewAllEmployeesByDepartment",
+        name: "View All Employees By Department",
+      },
+      {
+        short: "Employees By Role",
+        value: "viewAllEmployeesByRole",
+        name: "View All Employees By Role",
+      },
+      {
+        short: "Add Employee",
+        value: "addEmployee",
+        name: "Add an Employee",
+      },
+      {
+        short: "Remove Employee",
+        value: "removeEmployee",
+        name: "Remove an Employee",
+      },
+      {
+        value: "updateEmployee",
+        name: "Update an Employee",
+      },
+      {
+        value: "updateEmployeeRole",
+        name: "Update Employee Role",
+      },
+      {
+        value: "updateEmployeeManager",
+        name: "Update Employee Manager",
+      },
+      {
+        short: "Roles",
+        value: "viewAllRoles",
+        name: "View All Roles",
+      },
+      {
+        value: "addRole",
+        name: "Add Role",
+      },
+      {
+        value: "removeRole",
+        name: "Remove Role",
+      },
+      {
+        short: "Departments",
+        value: "viewAllDepartments",
+        name: "View All Departments",
+      },
+      {
+        value: "addDepartment",
+        name: "Add Departments",
+      },
+      {
+        value: "removeDepartment",
+        name: "Remove Departments",
+      },
+      {
+        short: "Budget",
+        value: "viewBudget",
+        name: "View Utilised Budget for a Department",
+      },
+      {
+        short: "Exit",
+        value: "exit",
+        name: "Exit",
+      },
     ],
   });
+
   const answers = await inquirer.prompt(question);
-  if (answers.action === "exit") {
+  if (answers.options === "exit") {
     inProgress = false;
   } else {
-    if (answers.action === "viewAllDepartments") {
+    if (answers.options === "View All Departments") {
       const query = "SELECT * FROM department";
       const data = await db.query(query);
       console.table(data);
+    } else {
     }
+  }
+  //Declaring add department function
+  async function addDepartment() {
+    const answers = await inquirer.prompt({
+      type: "input",
+      message: "What is the department this role belongs to?",
+      name: "department",
+    });
+    const newDepartment = answers.department;
+  }
+
+  //Declaring add employee function
+  async function addEmployee() {
+    const answers = await inquirer.prompt({
+      type: "input",
+      message: "Added Employee in the list",
+      name: "employee",
+    });
+    const newEmployee = answer.employee;
+  }
+
+  //Declaring add role function
+  async function addRole() {
+    const answers = await inquirer.prompt({
+      type: "input",
+      message: "Added Role in the list",
+      name: "role",
+    });
+    const newRole = answer.Role;
   }
 }
 init();
